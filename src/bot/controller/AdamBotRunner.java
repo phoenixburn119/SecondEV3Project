756990f6918123b3bbb667727a6a3d1c2d88d81b
Adam Kinzer <adamkinzer915@gmail.com>
2015-12-16 12:11:06 -0700

    Added Project

---
+++ src/bot/controller/AdamBotRunner.java
@@ -0,0 +1,10 @@
+package bot.controller;
+
+public class AdamBotRunner
+{
+	public static void main(String [] args)
+	{
+		RobotController myRobotcontroller = new RobotController();
+		myRobotController.start();
+	}
+}
---
+++ src/bot/controller/RobotController.java
@@ -0,0 +1,9 @@
+package bot.controller;
+
+public class RobotController
+{
+	public void start()
+	{
+		
+	}
+}
